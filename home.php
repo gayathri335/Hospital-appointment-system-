245c1c4f0fc69159c39fb62f9db1a7b7fefc7d4d
gayathri335 <121661839+gayathri335@users.noreply.github.com>
2023-06-27 23:29:45 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php include("head.html")
+?>
+<body>
+<div class="home">
+<div class="img">
+<img src=""C:\Users\hp\Desktop\Contact-page-Book-an-Appointment_Mobile-banner_578X364-px-02.jpg"" height="250px" width="1000px" />
+</div>
+<div class="heading">
+<h1>HOSPITAL APPOINTMENT BOOKING SYSTEM</h1>
+</div>
+<div class="para">
+<p>patients can use the platform to easily schedule appointments with their respective doctors.patients can cancel or reschedule their appointment as per their convenient.It is designed to be user-friendly to make appointments via laptop,tablets,smart phone.
+</p>
+</div>
+</div>
+</body>
+<?php include("foot.html")
+?>
